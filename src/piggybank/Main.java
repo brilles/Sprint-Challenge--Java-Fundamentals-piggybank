3bc3df439e886e4d490afef83ae4dcf671321c4a
Brian Illes <brianrilles@gmail.com>
2019-07-13 11:48:21 -0700

    Initial commit / set up

---
+++ src/piggybank/Main.java
@@ -0,0 +1,15 @@
+package piggybank;
+
+public class Main
+{
+	private static void workWithPiggyBank()
+	{
+		// TODO
+		System.out.println("Hello world(again)");
+	}
+
+	public static void main(String[] args)
+	{
+		workWithPiggyBank();
+	}
+}
